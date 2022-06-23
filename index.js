11069a99280d8a5c11d93115d941e4fd84d675d7
ApplesBear <78280793+ApplesBear@users.noreply.github.com>
2022-06-23 13:08:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -159,7 +159,7 @@ async function getRepositories() {
     let response = await octokit.request(`GET /orgs/${owner}/repos`);
     result = [...response.data];
 
-    while (response.headers.link?.search('next') !== -1) {
+    while (response.headers.link && response.headers.link.search('next') !== -1) {
         response = await octokit.request(`GET /orgs/${owner}/repos?page=${++page}`);
         result = [...result, ...response.data];
     }
@@ -173,7 +173,7 @@ async function getTags(repo_name) {
     let response = await octokit.request(`GET /repos/${owner}/${repo_name}/tags`);
     result = [...response.data];
 
-    while (response.headers.link?.search('next') !== -1) {
+    while (response.headers.link && response.headers.link.search('next') !== -1) {
         console.log('Searching for "frontend" tag in current repo...');
 
         response = await octokit.request(`GET /repos/${owner}/${repo_name}/tags?page=${++page}`);
