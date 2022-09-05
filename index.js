b161940062e7eb8770ebb864d2253f07ae1b1ddb
Onikiienko Bogdan <onikiienko@gmail.com>
2022-09-05 12:55:23 +0300

    feat: switched front-end repos filtering from labels to topic "frontend"

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Octokit } from "https://cdn.skypack.dev/octokit";
+import { Octokit } from 'https://cdn.skypack.dev/octokit';
 
 const startBtn = document.querySelector('button');
 const downloadBtn = document.querySelector('.download');
@@ -7,217 +7,212 @@ let owner, octokit;
 
 startBtn.addEventListener('click', techRadar);
 
-
 async function techRadar() {
-    startBtn.disabled = true;
-    downloadBtn.disabled = true;
-    state.innerText = 'Please, wait. You can monitor progress in console.';
+  startBtn.disabled = true;
+  downloadBtn.disabled = true;
+  state.innerText = '\n🏁 Please, wait';
 
-    setVariables();
+  setVariables();
 
-    const repositories = await getFrontendRepositories();
-    const packageJsonFiles = await getPackageJsonFiles(repositories);
-    const dependencies = await getAllDependencies(packageJsonFiles);
+  const repositories = await getFrontendRepositories();
+  const packageJsonFiles = await getPackageJsonFiles(repositories);
+  const dependencies = await getAllDependencies(packageJsonFiles);
 
-    if (Object.keys(dependencies).length > 0) {
-        enableResultDownloading(dependencies);
-    } else {
-        state.innerText = 'Sorry, we did not find any dependencies. Are you sure your repositories have "frontend" labels?';
-    }
+  if (Object.keys(dependencies).length > 0) {
+    enableResultDownloading(dependencies);
+  } else {
+    state.innerText =
+      '\n😔 Sorry, we did not find any dependencies. Are you sure your repositories have "frontend" topic?';
+  }
 
-    state.innerText = 'Done. Please, download your result.';
+  state.innerText += '\n ✅ Done. Please, download your result';
 }
 
 function setVariables() {
-    const token = document.querySelector('.token').value;
-    owner = document.querySelector('.owner').value;
-    octokit = new Octokit({
-        auth: token
-    });
+  const token = document.querySelector('.token').value;
+  owner = document.querySelector('.owner').value;
+  octokit = new Octokit({
+    auth: token,
+  });
 }
 
 async function getFrontendRepositories() {
-    console.log(`Getting repositories from ${owner}...`);
+  state.innerText += `\n 👷‍♀️ Getting repositories from ${owner}`;
 
-    const repositoriesArray = await getRepositories();
-    const result = [];
+  const repositoriesArray = await getRepositories();
+  let result = [];
 
-    console.log(`We found ${repositoriesArray.length} repositories.`);
+  state.innerText += `\n🥳 We've found ${repositoriesArray.length} repositories`;
+  state.innerText += '\n🕵️‍♀️ Filter frontend repositories';
 
-    for (let i = 0; i < repositoriesArray.length; i++) {
-        console.log('Filter frontend repositories...');
+  result = repositoriesArray.filter(({ topics, archived, name }) => topics.includes('frontend') && !archived);
 
-        const labelsArray = await getLabels(repositoriesArray[i].name);
+  state.innerText += `\n🥳 We found ${result.length} frontend repositories`;
 
-        labelsArray.find((label) => {
-          if (label.name === 'frontend') {
-            result.push(repositoriesArray[i]);
-            return true;
-          }
-        });
-    }
-
-    console.log(`We found ${result.length} frontend repositories.`);
-
-    return result;
+  return result;
 }
 
 async function getPackageJsonFiles(repositories) {
-    const packageJsons = [];
+  const packageJsons = [];
 
-    for (let i = 0; i < repositories.length; i++) {
-        console.log('Search for package.json files...');
+  for (let i = 0; i < repositories.length; i++) {
+    state.innerText += `\n🔎 Looking for package.json files in ${repositories[i].name}`;
 
-        const commitsArray = await getCommits(repositories[i].name);
+    const commitsArray = await getCommits(repositories[i].name);
 
-        const headCommit = commitsArray.shift();
+    const headCommit = commitsArray.shift();
 
-        const treeArray = await getTree(repositories[i].name, headCommit.commit.tree.sha);
+    const treeArray = await getTree(
+      repositories[i].name,
+      headCommit.commit.tree.sha
+    );
 
-        await parseNextTreeLvl(repositories[i], treeArray, packageJsons, 1);
-    }
+    await parseNextTreeLvl(repositories[i], treeArray, packageJsons, 1);
+  }
 
-    console.log(`We found ${packageJsons.length} package.json files.`);
+  state.innerText += `\n🥳 We've found ${packageJsons.length} package.json files`;
 
-    return packageJsons;
+  return packageJsons;
 }
 
 async function parseNextTreeLvl(repo, tree = [], results, depth) {
-    const treeArray = tree.filter((element) => {
-        if (element.path === 'package.json') {
-            results.push({
-              repo_name: repo.name,
-              sha: element.sha});
-        }
-
-        return element.type === 'tree';
-    });
-
-    if (depth > 3) {
-        return;
+  const treeArray = tree.filter((element) => {
+    if (element.path === 'package.json') {
+      results.push({
+        repo_name: repo.name,
+        sha: element.sha,
+      });
     }
 
-    depth++;
+    return element.type === 'tree';
+  });
 
-    for (let i = 0; i < treeArray.length; i++) {
-        const nextTreeArray = await getTree(repo.name, treeArray[i].sha);
-        await parseNextTreeLvl(repo, nextTreeArray, results, depth);
-    }
+  if (depth > 3) {
+    return;
+  }
+
+  depth++;
+
+  for (let i = 0; i < treeArray.length; i++) {
+    const nextTreeArray = await getTree(repo.name, treeArray[i].sha);
+    await parseNextTreeLvl(repo, nextTreeArray, results, depth);
+  }
 }
 
 async function getAllDependencies(packageJsons) {
-    const dependencies = {};
+  const dependencies = {};
 
-    for (let i = 0; i < packageJsons.length; i++) {
-        console.log('Parsing package.json file...');
+  for (let i = 0; i < packageJsons.length; i++) {
+    state.innerText += `\n 🤔 Parsing package.json file in repo ${packageJsons[i].repo_name}`;
 
-        const file = await getPackageJson(packageJsons[i].repo_name, packageJsons[i].sha);
-        const json = JSON.parse(atob(file.content));
+    const file = await getPackageJson(
+      packageJsons[i].repo_name,
+      packageJsons[i].sha
+    );
+    const json = JSON.parse(atob(file.content));
 
-        getDependenciesFromJson(packageJsons[i].repo_name, json, dependencies);
-    }
+    getDependenciesFromJson(packageJsons[i].repo_name, json, dependencies);
+  }
 
-    console.log(`We found ${Object.keys(dependencies).length} dependencies.`);
+  state.innerText += `\n We've found ${
+    Object.keys(dependencies).length
+  } dependencies.`;
 
-    return dependencies;
+  return dependencies;
 }
 
-
 function getDependenciesFromJson(repo_name, json, dependencies) {
-
-    for (const dependency in json.dependencies) {
-        if (!(dependency in dependencies)) {
-            dependencies[dependency] = {
-              uses: 0,
-              versions: [],
-              repos: []
-            };
-        }
-        dependencies[dependency].uses++;
-        dependencies[dependency].versions.push(json.dependencies[dependency]);
-        dependencies[dependency].repos.push({
-          name: repo_name,
-          version: json.dependencies[dependency]
-        });
+  for (const dependency in json.dependencies) {
+    if (!(dependency in dependencies)) {
+      dependencies[dependency] = {
+        uses: 0,
+        versions: [],
+        repos: [],
+      };
     }
+    dependencies[dependency].uses++;
+    dependencies[dependency].versions.push(json.dependencies[dependency]);
+    dependencies[dependency].repos.push({
+      name: repo_name,
+      version: json.dependencies[dependency],
+    });
+  }
 
-    for (const dependency in json.devDependencies) {
-        if (!(dependency in dependencies)) {
-            dependencies[dependency] = {
-              uses: 0,
-              versions: [],
-              repos: []
-            };
-        }
-        dependencies[dependency].uses++;
-        dependencies[dependency].versions.push(json.devDependencies[dependency]);
-        dependencies[dependency].versions = [...new Set(dependencies[dependency].versions)];
-        dependencies[dependency].repos.push({
-          name: repo_name,
-          version: json.devDependencies[dependency]
-        });
+  for (const dependency in json.devDependencies) {
+    if (!(dependency in dependencies)) {
+      dependencies[dependency] = {
+        uses: 0,
+        versions: [],
+        repos: [],
+      };
     }
+    dependencies[dependency].uses++;
+    dependencies[dependency].versions.push(json.devDependencies[dependency]);
+    dependencies[dependency].versions = [
+      ...new Set(dependencies[dependency].versions),
+    ];
+    dependencies[dependency].repos.push({
+      name: repo_name,
+      version: json.devDependencies[dependency],
+    });
+  }
 }
 
 function enableResultDownloading(result) {
-    const string_result = JSON.stringify(result, undefined, 2);
-    const blob_result = new Blob([string_result], {type: 'application/json'});
+  const string_result = JSON.stringify(result, undefined, 2);
+  const blob_result = new Blob([string_result], {
+    type: 'application/json',
+  });
 
-    const link = document.createElement('a');
-    link.download = `${owner}-frontend-dependencies.json`;
-    link.href = window.URL.createObjectURL(blob_result);
+  const link = document.createElement('a');
+  link.download = `${owner}-frontend-dependencies.json`;
+  link.href = window.URL.createObjectURL(blob_result);
 
-    startBtn.disabled = false;
-    downloadBtn.disabled = false;
-    downloadBtn.addEventListener('click', () => {
-        link.click();
-    })
+  startBtn.disabled = false;
+  downloadBtn.disabled = false;
+  downloadBtn.addEventListener('click', () => {
+    link.click();
+  });
 
-    console.log('Done. Now you can download your results.');
+  state.innerText += `\n💾 Done. Now you can download your results`;
 }
 
 async function getRepositories() {
-    let page = 1;
-    let result = [];
-    let response = await octokit.request(`GET /orgs/${owner}/repos?per_page=100`);
-    result = [...response.data];
+  let page = 1;
+  let result = [];
+  let response = await octokit.request(`GET /orgs/${owner}/repos?per_page=100`);
+  result = [...response.data];
 
-    while (response.headers.link && response.headers.link.search('next') !== -1) {
-        response = await octokit.request(`GET /orgs/${owner}/repos?per_page=100&page=${++page}`);
-        result = [...result, ...response.data];
-    }
+  while (response.headers.link && response.headers.link.search('next') !== -1) {
+    response = await octokit.request(
+      `GET /orgs/${owner}/repos?per_page=100&page=${++page}`
+    );
+    result = [...result, ...response.data];
+  }
 
-    return result;
-}
-
-async function getLabels(repo_name) {
-    let page = 1;
-    let result = [];
-    let response = await octokit.request(`GET /repos/${owner}/${repo_name}/labels?per_page=100`);
-    result = [...response.data];
-
-    while (response.headers.link && response.headers.link.search('next') !== -1) {
-        response = await octokit.request(`GET /repos/${owner}/${repo_name}/labels?per_page=100&page=${++page}`);
-        result = [...result, ...response.data];
-    }
-
-    return result;
+  return result;
 }
 
 async function getCommits(repo_name) {
-    const response = await octokit.request(`GET /repos/${owner}/${repo_name}/commits`);
+  const response = await octokit.request(
+    `GET /repos/${owner}/${repo_name}/commits`
+  );
 
-    return response.data;
+  return response.data;
 }
 
 async function getTree(repo_name, tree_sha) {
-    const response = await octokit.request(`GET /repos/${owner}/${repo_name}/git/trees/${tree_sha}`);
+  const response = await octokit.request(
+    `GET /repos/${owner}/${repo_name}/git/trees/${tree_sha}`
+  );
 
-    return response.data.tree;
+  return response.data.tree;
 }
 
 async function getPackageJson(repo_name, packageFile_sha) {
-    const response = await octokit.request(`GET /repos/${owner}/${repo_name}/git/blobs/${packageFile_sha}`);
+  const response = await octokit.request(
+    `GET /repos/${owner}/${repo_name}/git/blobs/${packageFile_sha}`
+  );
 
-    return response.data;
+  return response.data;
 }
